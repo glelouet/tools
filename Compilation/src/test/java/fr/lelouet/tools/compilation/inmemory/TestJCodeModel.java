710ee826168736dd0e1a9e0962d9a2f9db89e1a4
glelouet <guillaume.lelouet@gmail.com>
2019-10-09 17:17:11 +0200

    added test on jcomdemodel

--- Compilation/src/test/java/fr/lelouet/tools/compilation/inmemory/TestJCodeModel.java
+++ Compilation/src/test/java/fr/lelouet/tools/compilation/inmemory/TestJCodeModel.java
@@ -13,7 +13,7 @@ import com.helger.jcodemodel.JExpr;
 import com.helger.jcodemodel.JMethod;
 import com.helger.jcodemodel.JMod;
 
-public class TestEqualsObject {
+public class TestJCodeModel {
 
 	// ignored because this is a bug in jcodemodel
 	// @org.testng.annotations.Test
@@ -32,6 +32,19 @@ public class TestEqualsObject {
 		Method createdmethod = Stream.of(createdclass.getMethods()).filter(m -> m.getName().equals(mt.name())).findFirst()
 				.get();
 		Assert.assertEquals(createdmethod.getParameters()[0].getType(), Object.class);
+	}
+
+	// ignored because this is a bug in jcodemodel
+	// @org.testng.annotations.Test
+	public void testBadPackage() throws JClassAlreadyExistsException, ClassNotFoundException {
+		JCodeModel cm = new JCodeModel();
+		JDefinedClass myclass = cm._class(JMod.PUBLIC, "mypck.MyClass");
+		JDefinedClass myObject = myclass._class(JMod.PUBLIC | JMod.STATIC, "Object");
+		myclass.method(JMod.PUBLIC, cm.VOID, "call").param(Object.class, "obj");
+		myclass.method(JMod.PUBLIC, cm.VOID, "call").param(myObject, "obj");
+
+		DynamicClassLoader dcl = DynamicClassLoader.generate(cm);
+		dcl.findClass(myclass.fullName());
 	}
 
 }
