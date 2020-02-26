a05163a9d866ea9fd76a2ff45544e5339361be9c
glelouet <guillaume.lelouet@gmail.com>
2020-02-26 15:02:46 +0100

    small comment

--- YamlSettings/src/main/java/fr/lelouet/tools/settings/ObsStructTools.java
+++ YamlSettings/src/main/java/fr/lelouet/tools/settings/ObsStructTools.java
@@ -3,10 +3,13 @@ package fr.lelouet.tools.settings;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-/** observale structure tools */
+/** observable structure tools */
 public class ObsStructTools {
 
-	/** copy the Observable fields from origin to dest */
+	/**
+	 * copy the Observable fields from origin to dest <br />
+	 * invoke all setX on the dest iff there is a getX in the origing
+	 */
 	public static <T> void copy(T origin, T dest) {
 		for (Method meth : dest.getClass().getMethods()) {
 			if (meth.getName().startsWith("set")) {
