09d7e5828e98de43f6a36abbd5b34910c6731005
glelouet <guillaume.lelouet@gmail.com>
2022-10-16 13:24:24 +0200

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#usage : VERSION=A.B.C ./deploy.sh
+
+if [ -z ${VERSION+x} ]; then echo "VERSION is unset"; exit 1 ; fi
+
+git pull && git add .
+
+mvn versions:set -DnewVersion=$VERSION && mvn deploy -T2C
+
+git commit -m "v$VERSION"\
+ && git tag "$VERSION"\
+ && git push\
+ && git push origin "$VERSION" 
